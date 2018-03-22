fdebc2beee0e82296b4d6d47baf526eeaf61525a
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2018-03-22 14:43:35 +0100

    Removed DbRoute class

--- src/main/java/pl/datamatica/traccar/model/Route.java
+++ src/main/java/pl/datamatica/traccar/model/Route.java
@@ -23,6 +23,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
@@ -30,14 +31,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.OrderColumn;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-@MappedSuperclass
+@Entity
+@Table(name="routes")
 public class Route implements IsSerializable, Cloneable {
     public static enum Status {
         NEW, IN_PROGRESS_OK, IN_PROGRESS_LATE,
